eb0171044c6d616e6ff09b9720b78b30d3c8090b
Laraib Iqbal <621833@pdsb.net>
2020-06-24 18:45:12 -0400

    - Calendar View

---
+++ MdvApp/MdvApp/DateCell.swift
@@ -0,0 +1,12 @@
+//
+//  DateCell.swift
+//  MdvApp
+//
+//  Created by School on 2020-06-24.
+//
+
+import JTAppleCalendar
+import UIKit
+class DateCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+}

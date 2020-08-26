943dc5c22eec59c5ec327e5fd11ade1efde968ee
Terry Latanville <terry.latanville@tulip.com>
2020-08-26 17:24:50 -0400

    Sample local notification permission checking

--- MdvApp/MdvApp/EventsDirectory.swift
+++ MdvApp/MdvApp/EventsDirectory.swift
@@ -13,99 +13,108 @@ struct Event: Hashable{
     let body: String
     let startDate : Date
     let endDate: Date
-   // let alert: 
+    // let alert:
 }
 
-    // MARK: - shows events body, start and end date
+// MARK: - shows events body, start and end date
 extension Event: ExpressibleByStringLiteral {
     init(stringLiteral value: String) {
         self.init(title: value, body: "", startDate: Date(), endDate: Date())
     }
 }
 
-    // MARK: - List of events
-class EventsDirectory {
+// MARK: - List of events
+class EventsDirectory: NSObject, UNUserNotificationCenterDelegate {
     var events: [Event] = []
     func addEvents(_ event: Event){
         events.append(event)
+        center.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized, .provisional:
+                self.scheduleNotification()
+            case .denied:
+                // Show an UIAlert
+                print("Check settings for notification setup")
+            case .notDetermined:
+                self.getUserPermission(success: self.scheduleNotification, failure: { print("D'Oh") })
+            @unknown default:
+                print("Unhandled auth type: \(settings.authorizationStatus)")
+            }
+        }
     }
     
     // MARK: - shows event on the date that user identified
-   func events(on date: Date) -> [Event] {
+    func events(on date: Date) -> [Event] {
         return events.filter { $0.startDate.hasSame(.day, as: date) }
     }
-}
 
-// MARK: - Notification Center
-let center = UNUserNotificationCenter.current()
 
-center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
-    if granted {
-        print("yay!")
-        
-    } else {
-                print("D'oh")
+    // MARK: - Notification Center
+    let center = UNUserNotificationCenter.current()
+    func getUserPermission(success: @escaping () -> Void, failure: @escaping () -> Void) {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                success()
+            } else {
+                failure()
+            }
+        }
+    }
+
+    func scheduleNotification() {
+        let center = UNUserNotificationCenter.current()
+
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = UNNotificationSound.default
+
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+
+        center.setNotificationCategories([category])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // pull out the buried userInfo dictionary
+        let userInfo = response.notification.request.content.userInfo
+
+        if let customData = userInfo["customData"] as? String {
+            print("Custom data received: \(customData)")
+
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                // the user swiped to unlock
+                print("Default identifier")
+
+            case "show":
+                // the user tapped our "show more info…" button
+                print("Show more information…")
+                break
+
+            default:
+                break
             }
         }
 
-func scheduleNotification() {
-    let center = UNUserNotificationCenter.current()
-
-    let content = UNMutableNotificationContent()
-    content.title = "Late wake up call"
-    content.body = "The early bird catches the worm, but the second mouse gets the cheese."
-    content.categoryIdentifier = "alarm"
-    content.userInfo = ["customData": "fizzbuzz"]
-    content.sound = UNNotificationSound.default
-
-    var dateComponents = DateComponents()
-    dateComponents.hour = 10
-    dateComponents.minute = 30
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-    center.add(request)
-}
-
-var dateComponents = DateComponents()
-dateComponents.hour = 10
-dateComponents.minute = 30
-let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-func registerCategories() {
-    let center = UNUserNotificationCenter.current()
-    center.delegate = self
-
-    let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
-    let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
-
-    center.setNotificationCategories([category])
-}
-
-func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-    // pull out the buried userInfo dictionary
-    let userInfo = response.notification.request.content.userInfo
-
-    if let customData = userInfo["customData"] as? String {
-        print("Custom data received: \(customData)")
-
-        switch response.actionIdentifier {
-        case UNNotificationDefaultActionIdentifier:
-            // the user swiped to unlock
-            print("Default identifier")
-
-        case "show":
-            // the user tapped our "show more info…" button
-            print("Show more information…")
-            break
-
-        default:
-            break
-        }
+        // you must call the completion handler when you're done
+        completionHandler()
     }
 
-    // you must call the completion handler when you're done
-    completionHandler()
+
 }
-
-

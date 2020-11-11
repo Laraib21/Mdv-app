c1025f0f75112046b6c8320fdd6f58ecf65fa1e3
Laraib <621833@pdsb.net>
2020-11-11 17:24:12 -0500

    n

--- MdvApp/MdvApp/NotificationManager.swift
+++ MdvApp/MdvApp/NotificationManager.swift
@@ -34,34 +34,13 @@ class NotificationManager: NSObject {
         
         NotificationManager.shared.center.setNotificationCategories([category])
     }
+    func applicationDidRecieve (_ ckQueryNotification: CKQueryNotification, completion: @escaping(Bool) -> Void) {
+        completion(true)
     
+    }
 }
 extension NotificationManager: UNUserNotificationCenterDelegate{
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        // pull out the buried userInfo dictionary
-        let userInfo = response.notification.request.content.userInfo
-        
-        if let customData = userInfo["customData"] as? String {
-            print("Custom data received: \(customData)")
-            
-            switch response.actionIdentifier {
-            case UNNotificationDefaultActionIdentifier:
-                // the user swiped to unlock
-                print("Default identifier")
-                
-            case "show":
-                // the user tapped our "show more info…" button
-                print("Show more information…")
-                break
-                
-            default:
-                break
-            }
-        }
-        
-        // you must call the completion handler when you're done
-        completionHandler()
-    }
+
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler(.banner)

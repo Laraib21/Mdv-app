e2f15acb6856e99986f92949abd6b1a0e9d994f6
Terry Latanville <terry.latanville@tulip.com>
2020-11-11 17:14:29 -0500

    Feature: Adds AppDelegate   - AppDelegate allows us to receive incoming push notifications

--- MdvApp/MdvApp/MdvApp.swift
+++ MdvApp/MdvApp/MdvApp.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 @main
 struct MdvApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -16,5 +18,16 @@ struct MdvApp: App {
                 }
         }
 
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // Handle receiving iCloud notification data
+        completionHandler(.noData) // TEMPORARY
     }
 }
